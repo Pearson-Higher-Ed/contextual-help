6bd89446d9c157b6423fb56c4356494c3293b9bf
Nicks <bob.nicks@pearson.com>
2018-01-31 10:47:47 -0700

    DES-846 Cross site security issue for help

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ module.exports = {
    dev_bundle: ['webpack/hot/dev-server', './main.js'],
    dist: ['./main.js']
   },
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+    }
+  },
   output: {
     path: './',
     filename: 'build/[name].contextual-help.js'
