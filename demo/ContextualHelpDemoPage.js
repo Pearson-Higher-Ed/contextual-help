355ccb0354524967698164ebbcf0fbd865d21187
Bob Nicks <bob.nicks@pearson.com>
2018-02-09 19:21:52 -0700

    DES-477 Integration with Drawer -not working

--- demo/ContextualHelpDemoPage.js
+++ demo/ContextualHelpDemoPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from '@pearson-components/elements-sdk/build/dist.elements-sdk';
 import { ContextualHelp, ContextualHelp1st } from '../index';
 
 import './ContextualHelpDemoPage.css';
@@ -7,7 +8,10 @@ class ContextualHelpDemoPage extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      drawerIsOpen: false,
+      position: 'left'
+    };
 
     setTimeout(() => {
       this.conHelp.addTopics('console/instructor/validatedinstructor');
@@ -28,12 +32,28 @@ class ContextualHelpDemoPage extends Component {
       ]);
     }, 1300);
   }
+//  <ContextualHelp ref={(instance) => { this.contextualHelp = instance }} />
+//  <ContextualHelp1st ref={(instance) => { this.conHelp = instance }} />
 
   render() {
     return (
 
       <div className="demo-container">
-        <ContextualHelp1st ref={(instance) => { this.conHelp = instance }} />
+
+
+        <Button btnType="cta" btnSize="xlarge" onClick={() => { this.contextualHelp.openDrawer(); }}>Toggle Drawer</Button>
+        <br />
+        <br />
+        <Button btnType="primary" btnSize="xlarge" onClick={() => {this.setState({position:"right"})}}>Drawer Position Right</Button>
+        <br />
+        <br />
+        <Button btnType="primary" btnSize="xlarge" onClick={() => {this.setState({position:"left"})}}>Drawer Position Left</Button>
+        <br />
+        <br />
+
+
+
+
         <ContextualHelp ref={(instance) => { this.contextualHelp = instance }} />
       </div>
 
--- src/js/ContextualHelp.js
+++ src/js/ContextualHelp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Drawer from '@pearson-components/drawer/main';
+import { Drawer, BasicView, DetailView } from '@pearson-components/drawer';
 import TopicsList from './TopicsList';
 
 class ContextualHelp extends Component {
@@ -7,8 +7,12 @@ class ContextualHelp extends Component {
     super(props);
 
     this.state = {
-      topics: []
+      topics: [],
+      drawerIsOpen: false
     };
+
+    this.drawerHandler = this._drawerHandler.bind(this);
+    this.openDrawer = this._openDrawer.bind(this);
 
     this.lang = props.language || 'en-us';
     this.helpTopicsList = new TopicsList(this._updateTopics, (topicName) => {
@@ -21,10 +25,33 @@ class ContextualHelp extends Component {
     this.setState({ topics: newList });
   }
 
+  _drawerHandler = () => {
+    this.setState({drawerIsOpen: !this.state.drawerIsOpen});
+  }
+
+  _openDrawer = () => {
+    this.setState({drawerIsOpen: true});
+  }
+
   render() {
     return (
       <div>
         <span>Contextual Help Component</span>
+
+        <Drawer drawerOpen={this.state.drawerIsOpen} position={'right'} headerTitle="Header Title" drawerHandler={this.drawerHandler} >
+          <div>
+            <BasicView mapToDetail='detailView1'>
+              <h2>Basic View 1</h2>
+              <ul>
+                <li>hi</li>
+                <li>there</li>
+              </ul>
+            </BasicView>
+            <DetailView id='detailView1'>
+              <h3>Detail View 1</h3>
+            </DetailView>
+          </div>
+        </Drawer>
       </div>
     )
   }
